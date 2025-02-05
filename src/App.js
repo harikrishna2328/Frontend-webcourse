8ff6c7f6e62c1402e17e21d4345eb1f3032d3ad8
Sainath yadav <harikrishna121c@gmail.com>
2025-02-05 23:51:07 +0530

    new project files added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hi this is h1 tag</h1>
+    </div>
+  );
+}
+
+export default App;
